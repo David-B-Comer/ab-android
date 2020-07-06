60577cea095ab7d67c2e6c29ceec1e90231ff26d
David-B-Comer <davec2233@gmail.com>
2020-07-06 09:46:13 -0400

    loading on android but not connecting to firebase

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:flutter_facebook_login/flutter_facebook_login.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:flutter_signin_button/flutter_signin_button.dart';
 
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
